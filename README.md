c6da6118ff69659036f730589cba300ad06a38f0
Juanfrancisco15 <juanfran.nbjs80@gmail.com>
2020-11-14 14:14:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bserrang-ejercicio-4-i6axhu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bserrang-ejercicio-4-i6axhu)
---
+++ script.js
@@ -0,0 +1,17 @@
+// En este ejercicio tenéis que sustituir TODAS las funciones tradicionales por funciones flecha y que siga funcionando igual.
+
+const changeBackgroundButton = document.getElementById(
+  "changeBackgroundButton"
+);
+
+changeBackgroundButton.addEventListener("click", ()=> {
+  this.style.backgroundColor = ()=>{
+      const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+  };
+});
+
